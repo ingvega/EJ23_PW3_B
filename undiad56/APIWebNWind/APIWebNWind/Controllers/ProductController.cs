5dec2bde07c7c2be7ec7cbe093df5370ada9b32d
ingvega <isc.p.vega@gmail.com>
2023-05-18 14:06:48 -0600

    Consultas con join

--- undiad56/APIWebNWind/APIWebNWind/Controllers/ProductController.cs
+++ undiad56/APIWebNWind/APIWebNWind/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using APIWebNWind.Data;
 using APIWebNWind.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace APIWebNWind.Controllers
 {
@@ -28,14 +29,14 @@ namespace APIWebNWind.Controllers
         [Route("GetActivos")]
         public IEnumerable<Product> GetActivos()
         {
-            Product p = new Product();
+            /*Product p = new Product();
             p.ProductName = "X";
             p.UnitPrice = 20;
             Product p1 = new Product()
             {
                 ProductName = "X",
                 UnitPrice = 20
-            };
+            };*/
 
             IEnumerable<Product> lista =
                  from prod in contexto.Products
@@ -45,7 +46,6 @@ namespace APIWebNWind.Controllers
                      ProductName = prod.ProductName,
                      UnitPrice = prod.UnitPrice
                  };
-
             return lista;
         }
 
@@ -53,15 +53,36 @@ namespace APIWebNWind.Controllers
         [Route("GetActivos2")]
         public IEnumerable<Object> GetActivos2()
         {
-
             IEnumerable<Object> lista =
                  from prod in contexto.Products
+                 
                  where prod.Discontinued == false
                  select new
                  {
                      Name = prod.ProductName,
-                     Price = prod.UnitPrice
+                     Price = prod.UnitPrice,
+                     Categoria=prod.Category.CategoryName
                  };
+
+            return lista;
+        }
+
+        [HttpGet]
+        [Route("GetActivos2Metodos")]
+        public IEnumerable<Object> GetActivos2Metodos()
+        {
+            IEnumerable<Object> lista =
+                 contexto.Products.
+                 Where(producto => producto.Discontinued == false)
+                 .Join(contexto.Categories,
+                 producto=>producto.CategoryId,
+                 categoria=>categoria.CategoryId,
+                (p, c) => new {
+                     Name = p.ProductName,
+                     Price = p.UnitPrice,
+                     Categoria = c.CategoryName
+                 });
+                 
 
             return lista;
         }
