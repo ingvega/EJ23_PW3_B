8c0c5194e38f3b259f54253c3a3e08f99b1db852
ingvega <isc.p.vega@gmail.com>
2023-05-22 14:37:29 -0600

    Añadir páginas para gráfica y tablas

--- undiad56/APIWebNWind/APIWebNWind/Controllers/ProductController.cs
+++ undiad56/APIWebNWind/APIWebNWind/Controllers/ProductController.cs
@@ -54,14 +54,17 @@ namespace APIWebNWind.Controllers
         public IEnumerable<Object> GetActivos2()
         {
             IEnumerable<Object> lista =
+                //from Products prod
                  from prod in contexto.Products
-                 
+                 //join Categories cat on prod.CategoryId=cat.CategoryId
+                 join cat in contexto.Categories on prod.CategoryId equals cat.CategoryId
                  where prod.Discontinued == false
                  select new
                  {
                      Name = prod.ProductName,
                      Price = prod.UnitPrice,
-                     Categoria=prod.Category.CategoryName
+                     Category = cat.CategoryName 
+                     //Categoria=prod.Category.CategoryName
                  };
 
             return lista;
@@ -71,20 +74,45 @@ namespace APIWebNWind.Controllers
         [Route("GetActivos2Metodos")]
         public IEnumerable<Object> GetActivos2Metodos()
         {
+            //"Programacion Web".Replace("r","R").Replace(" ","-").Replace("r","R")
             IEnumerable<Object> lista =
-                 contexto.Products.
-                 Where(producto => producto.Discontinued == false)
+                 contexto.Products
+                 .Where(producto => producto.Discontinued == false )
                  .Join(contexto.Categories,
-                 producto=>producto.CategoryId,
-                 categoria=>categoria.CategoryId,
-                (p, c) => new {
-                     Name = p.ProductName,
-                     Price = p.UnitPrice,
-                     Categoria = c.CategoryName
-                 });
+                    producto=>producto.CategoryId,
+                    categoria=>categoria.CategoryId,
+                    (p, c) =>  new {
+                        Nombre = p.ProductName,
+                        Precio = p.UnitPrice,
+                        Categoria = c.CategoryName,
+                        Unidades = p.UnitsInStock
+                    })
+                 .Where(x=> x.Unidades>0);
                  
 
             return lista;
         }
+
+        [HttpGet]
+        [Route("InventarioXCategoria")]
+        public IEnumerable<Object> GetInventarioPorCategoria()
+        {
+            IEnumerable<Object> lista =
+                 //from Products prod
+                 from prod in contexto.Products
+                 where prod.Discontinued == false
+                 //join Categories cat on prod.CategoryId=cat.CategoryId
+                 join cat in contexto.Categories on prod.CategoryId equals cat.CategoryId
+                 group new { prod, cat } by cat.CategoryName into prod_cat
+                 select new
+                 {
+                     Categoria = prod_cat.Key,
+                     Productos = prod_cat.Count(),
+                     ValorInventario = prod_cat.Sum(pc=>pc.prod.UnitPrice*pc.prod.UnitsInStock)
+                 };
+
+            return lista;
+        }
+
     }
 }
